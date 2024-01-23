38acb94db796d3127542499c60be5dbbcd19f289
Douglas360 <douglas_henriqueduarte@hotmail.com>
2024-01-23 08:07:23 -0300

    Add Service Section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,13 @@ import React from 'react'
 
 //INTERNAL IMPORT
 import Style from '../styles/index.module.css'
-import { HeroSection } from '../components/componentsindex'
+import { HeroSection, Service } from '../components/componentsindex'
 
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection />
+      <Service />
     </div>
   )
 }
